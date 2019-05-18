d060032ae868a14ed8ebea328f15596aa0251339
ynakagawa33 <exrumya@gmail.com>
2019-05-19 07:41:36 +0900

    Modified: キーマップを更新した

--- keyboards/yosino58/keymaps/mine/keymap.c
+++ keyboards/yosino58/keymaps/mine/keymap.c
@@ -46,7 +46,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|                                  |------+------+------+------+------+------|
  * |LCTRL |   Z  |   X  |   C  |   V  |   B  |-------.-------.  ,---------------|   N  |   M  |   ,  |   .  |   /  |  \   |
  * `-----------------------------------------/   [   /       /   \       \   ]   \----------------------------------------'
- *                          | LAlt | LGUI | /-------/ Space /     \ Enter \-------\  | Bksp |RShift|
+ *                          | LAlt | LGUI | /-------/ Space /     \ Enter \-------\  | Bksp | LCAG |
  *                          |      |      |/ LOWER /       /       \       \ RAISE \ |      |      |
  *                          `-----------------------------'         '------------------------------'
  */
@@ -55,7 +55,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,   KC_Q, KC_W, KC_E, KC_R,    KC_T,                                       KC_Y,    KC_U,    KC_I,    KC_O,   KC_P,    KC_EQL,  \
   KC_LSFT,  KC_A, KC_S, KC_D, KC_F,    KC_G,                                       KC_H,    KC_J,    KC_K,    KC_L,   KC_SCLN, KC_QUOT, \
   KC_LCTRL, KC_Z, KC_X, KC_C, KC_V,    KC_B,    KC_LBRC, KC_SPC, KC_ENT, KC_RBRC,  KC_N,    KC_M,    KC_COMM, KC_DOT, KC_SLSH, KC_BSLS, \
-                              KC_LALT, KC_LGUI, CK_EMHL,                 CK_KHKR,  KC_BSPC, KC_RSFT\
+                              KC_LALT, KC_LGUI, CK_EMHL,                 CK_KHKR,  KC_BSPC, LCAG(KC_NO)\
   ),
 
 /* LOWER
