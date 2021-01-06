4bd1df0ef52f9a89a92d43a71a9df81a3ca4e8f1
ynakagawa33 <exrumya@gmail.com>
2021-01-06 17:48:19 +0900

    Modified: Alfred 用の起動ショートカットを中央のキーに登録した

--- keyboards/bat43/keymaps/ynakagawa33/keymap.c
+++ keyboards/bat43/keymaps/ynakagawa33/keymap.c
@@ -34,7 +34,7 @@ enum custom_keycodes {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_BASE] = LAYOUT(
         ALT_T(KC_TAB),  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,             KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    ALT_T(KC_EQL),
-        SFT_T(KC_LBRC), KC_A,    KC_S,    KC_D,    KC_F,    KC_G,  XXXXXXX,   KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, SFT_T(KC_QUOT),
+        SFT_T(KC_LBRC), KC_A,    KC_S,    KC_D,    KC_F,    KC_G,  LCAG(KC_SPC),   KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, SFT_T(KC_QUOT),
         CTL_T(KC_RBRC), KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,             KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, CTL_T(KC_BSLS),
                             KC_LGUI,CK_EMHS,LT(_LOWER, KC_SPC),             LT(_ADJUST, KC_ENT),CK_KHKR,KC_BSPC,
                                             XXXXXXX, XXXXXXX,               XXXXXXX, XXXXXXX, XXXXXXX
