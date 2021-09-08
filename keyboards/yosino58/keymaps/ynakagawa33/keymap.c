18bbc8dc03841b7269ca06170a612ca8a4731c62
ynakagawa33 <exrumya@gmail.com>
2021-09-08 14:34:23 +0900

    Modified: yosino58 の RAISE と LOWWE のレイヤーを入れ替えた

--- keyboards/yosino58/keymaps/ynakagawa33/keymap.c
+++ keyboards/yosino58/keymaps/ynakagawa33/keymap.c
@@ -65,7 +65,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                          |      |      |/  EMHS /       /       \       \ KHKR  \ |      |      |
  *                          `-----------------------------'         '------------------------------'
  */
- [_LOWER] = LAYOUT( \
+ [_RAISE] = LAYOUT( \
   KC_ESC,  KC_F1,      KC_F2,         KC_F3,         KC_F4,      KC_F5,                                        KC_F6,   KC_F7,      KC_F8,   KC_F9,   KC_F10,  KC_F11, \
   KC_LALT, XXXXXXX,    LCAG(KC_W),    LCAG(KC_M),    LCAG(KC_R), LCAG(KC_SPC),                                 KC_PGUP, KC_HOME,    KC_UP,   KC_END,  KC_PSCR, XXXXXXX, \
   KC_LSFT, XXXXXXX,    LCAG(KC_S),    LCAG(KC_D),    LCAG(KC_F), XXXXXXX,                                      KC_PGDN, KC_LEFT,    KC_DOWN, KC_RGHT, KC_PAUS, KC_INS, \
@@ -87,7 +87,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                          |      |      |/  EMHS /       /       \       \  KHKR \ |      |      |
  *                          `-----------------------------'         '------------------------------'
  */
- [_RAISE] = LAYOUT( \
+ [_LOWER] = LAYOUT( \
   KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,    KC_F5,                                       KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11, \
   KC_LALT, XXXXXXX, KC_ACL0, KC_ACL1, KC_ACL2,  XXXXXXX,                                     KC_WH_U, KC_WH_L, KC_MS_U, KC_WH_R, XXXXXXX, XXXXXXX, \
   KC_LSFT, XXXXXXX, KC_BTN2, KC_BTN3, KC_BTN1,  XXXXXXX,                                     KC_WH_D, KC_MS_L, KC_MS_D, KC_MS_R, XXXXXXX, XXXXXXX, \
