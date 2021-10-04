e6c1648ceb3d576bdc875349af846bb9e6481d44
Yasuo Nakagawa <y.nakagawa@sansan.com>
2021-10-04 09:37:46 +0900

    Modified: ディレクトリ構造の変更

--- keyboards/claw44/keymaps/ynakagawa33/keymap.c
+++ keyboards/claw44/keymaps/ynakagawa33/keymap.c
@@ -16,15 +16,17 @@ extern uint8_t is_master;
 // Layer names don't all need to be of the same length, obviously, and you can also skip them
 // entirely and just use numbers.
 
+// Defines names for use in layer keycodes and the keymap
+enum layer_names {
+  _QWERTY,
+  _LOWER,
+  _RAISE,
+  _ADJUST
+};
+
 enum custom_keycodes {
-  QWERTY = SAFE_RANGE,
-  LOWER,
-  RAISE,
-  ADJUST,
-  CK_KHKR,
-  CK_EMHS,
-  CK_SPCL,
-  CK_ENTA
+  CK_KHKR = SAFE_RANGE,
+  CK_EMHS
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -42,15 +44,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
 
   [_LOWER] = LAYOUT( \
-  //,--------+-----------+--------------+--------------+--------------+---------.   ,--------+-----------+--------+--------+--------+--------.
-     KC_LALT, XXXXXXX,    XXXXXXX,       LCAG(KC_UP),   XXXXXXX,       XXXXXXX,      KC_PGUP, KC_HOME,    KC_UP,   KC_END,  KC_PSCR, XXXXXXX,
-  //|--------+-----------+--------------+--------------+--------------+---------|   |--------+-----------+--------+--------+--------+--------|
-     KC_LSFT, XXXXXXX,    LCAG(KC_LEFT), LCAG(KC_DOWN), LCAG(KC_RGHT), XXXXXXX,      KC_PGDN, KC_LEFT,    KC_DOWN, KC_RGHT, KC_PAUS, KC_INS,
-  //|--------+-----------+--------------+--------------+--------------+---------|   |--------+-----------+--------+--------+--------+--------|
-     KC_LCTL, LCAG(KC_1), LCAG(KC_2),    LCAG(KC_3),    LCAG(KC_4),    KC_BSPC,      XXXXXXX, LCAG(KC_M), XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-  //`--------+-----------+--------------+--------------+--------------+---------/   \--------+-----------+--------+--------+--------+--------'
-                          _______,       _______,        _______,      _______,      _______, _______,    _______, _______
-  //                     `--------------+--------------+--------------+--------'    `--------+-----------+--------+--------'
+  //,--------+-----------+-----------+-----------+-----------+---------.   ,--------+--------+--------+--------+--------+--------.
+     KC_LALT, XXXXXXX,    LCAG(KC_W), LCAG(KC_M), LCAG(KC_R), LCAG(KC_SPC), KC_PGUP, KC_HOME, KC_UP,   KC_END,  KC_PSCR, XXXXXXX,
+  //|--------+-----------+-----------+-----------+-----------+---------|   |--------+--------+--------+--------+--------+--------|
+     KC_LSFT, XXXXXXX,    LCAG(KC_S), LCAG(KC_D), LCAG(KC_F), XXXXXXX,      KC_PGDN, KC_LEFT, KC_DOWN, KC_RGHT, KC_PAUS, KC_INS,
+  //|--------+-----------+-----------+-----------+-----------+---------|   |--------+--------+--------+--------+--------+--------|
+     KC_LCTL, LCAG(KC_1), LCAG(KC_2), LCAG(KC_3), LCAG(KC_4), LCAG(KC_C),   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_F12,
+  //`--------+-----------+-----------+-----------+-----------+---------/   \--------+--------+--------+--------+--------+--------'
+                          _______,    _______,    _______,    _______,      _______, _______, _______, _______
+  //                     `-----------+-----------+-----------+--------'    `--------+--------+--------+--------'
   ),
 
   [_RAISE] = LAYOUT( \
@@ -59,7 +61,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //|--------+--------+--------+--------+--------+--------|   |--------+--------+--------+--------+--------+--------|
      KC_LSFT, XXXXXXX, KC_BTN2, KC_BTN3, KC_BTN1, XXXXXXX,     KC_WH_D, KC_MS_L, KC_MS_D, KC_MS_R, XXXXXXX, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------|   |--------+--------+--------+--------+--------+--------|
-     KC_LCTL, _______, _______, _______, _______, KC_BSPC,     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+     KC_LCTL, _______, _______, _______, _______, XXXXXXX,     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_F12,
   //`--------+--------+--------+--------+--------+--------/   \--------+--------+--------+--------+--------+--------'
                        _______, _______, _______, _______,     _______, _______, _______, _______
   //                  `--------+--------+--------+--------'   `--------+--------+--------+--------'
@@ -71,7 +73,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //|--------------+--------+--------+--------+--------+--------|   |--------+--------+--------+--------+--------+--------|
      SFT_T(KC_GRV), KC_1,    KC_2,    KC_3,    KC_4,    KC_5,        KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,
   //|--------------+--------+--------+--------+--------+--------|   |--------+--------+--------+--------+--------+--------|
-     KC_LCTL,       _______, _______, _______, _______, KC_BSPC,     XXXXXXX, XXXXXXX, XXXXXXX, KC_DOT,  XXXXXXX, KC_F12,
+     KC_LCTL,       _______, _______, _______, _______, XXXXXXX,     XXXXXXX, XXXXXXX, XXXXXXX, KC_DOT,  XXXXXXX, KC_F12,
   //`--------------+--------+--------+--------+--------+--------/   \--------+--------+--------+--------+--------+--------'
                              _______, _______, _______, _______,     _______, _______, _______, _______
   //                        `--------+--------+--------+--------'   `--------+--------+--------+--------'
@@ -81,7 +83,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 void matrix_init_user(void) {
   //SSD1306 OLED init, make sure to add #define SSD1306OLED in config.h
   #ifdef SSD1306OLED
-    iota_gfx_init(!has_usb());   // turns on the display
+  iota_gfx_init();   // turns on the display
   #endif
 }
 
@@ -107,9 +109,9 @@ void matrix_scan_user(void) {
 void matrix_render_user(struct CharacterMatrix *matrix) {
   if (is_master) {
     // If you want to change the display of OLED, you need to change here
-    matrix_write_ln(matrix, read_layer_state());
-    matrix_write_ln(matrix, read_keylog());
-    matrix_write_ln(matrix, read_keylogs());
+    matrix_write_P(matrix, read_layer_state());
+    matrix_write_P(matrix, read_keylog());
+    matrix_write_P(matrix, read_keylogs());
     //matrix_write_ln(matrix, read_mode_icon(keymap_config.swap_lalt_lgui));
     //matrix_write_ln(matrix, read_host_led_state());
     //matrix_write_ln(matrix, read_timelog());
@@ -137,10 +139,6 @@ static bool raise_pressed = false;
 static uint16_t raise_pressed_time = 0;
 static bool shift_pressed = false;
 static uint16_t shift_pressed_time = 0;
-static bool lower_pressed = false;
-static uint16_t lower_pressed_time = 0;
-static bool adjust_pressed = false;
-static uint16_t adjust_pressed_time = 0;
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   if (record->event.pressed) {
@@ -151,36 +149,6 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   }
 
   switch (keycode) {
-    case QWERTY:
-      if (record->event.pressed) {
-        set_single_persistent_default_layer(_QWERTY);
-      }
-      return false;
-      break;
-    case LOWER:
-      if (record->event.pressed) {
-        layer_on(_LOWER);
-      } else {
-        layer_off(_LOWER);
-      }
-      return false;
-      break;
-    case RAISE:
-      if (record->event.pressed) {
-        layer_on(_RAISE);
-      } else {
-        layer_off(_RAISE);
-      }
-      return false;
-      break;
-   case ADJUST:
-     if (record->event.pressed) {
-       layer_on(_ADJUST);
-     } else {
-       layer_off(_ADJUST);
-     }
-     return false;
-     break;
    case CK_KHKR:
     if (record->event.pressed) {
       raise_pressed = true;
@@ -216,40 +184,6 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
       }
 
       shift_pressed = false;
-    }
-    return false;
-    break;
-    case CK_SPCL:
-    if (record->event.pressed) {
-      lower_pressed = true;
-      lower_pressed_time = record->event.time;
-      layer_on(_LOWER);
-    } else {
-      layer_off(_LOWER);
-
-      if (lower_pressed && (TIMER_DIFF_16(record->event.time, lower_pressed_time) < TAPPING_TERM)) {
-        register_code(KC_SPC);
-        unregister_code(KC_SPC);
-      }
-
-      lower_pressed = false;
-    }
-    return false;
-    break;
-    case CK_ENTA:
-    if (record->event.pressed) {
-      adjust_pressed = true;
-      adjust_pressed_time = record->event.time;
-      layer_on(_ADJUST);
-    } else {
-      layer_off(_ADJUST);
-
-      if (adjust_pressed && (TIMER_DIFF_16(record->event.time, adjust_pressed_time) < TAPPING_TERM)) {
-        register_code(KC_ENT);
-        unregister_code(KC_ENT);
-      }
-
-      adjust_pressed = false;
     }
     return false;
     break;
