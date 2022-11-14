32714f9a0a4bd026f79f11f4af156cc87e7b9679
ynakagawa33 <exrumya@gmail.com>
2022-11-14 22:50:06 +0900

    Modified: Alt+Tab が利用できるようにキーマップを調整した

--- keyboards/projectkb/alice/keymaps/ynakagawa33/keymap.c
+++ keyboards/projectkb/alice/keymaps/ynakagawa33/keymap.c
@@ -31,7 +31,7 @@ enum custom_keycodes {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_BASE] = LAYOUT_alice_split_bs(
     KC_ESC,  KC_GRV, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,   KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_DEL,  KC_BSPC,
-    KC_PGUP, ALT_T(KC_TAB),  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,             KC_Y,   KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,
+    KC_TAB, ALT_T(KC_TAB),  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,             KC_Y,   KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,
     KC_PGDN, SFT_T(KC_LBRC), KC_A,    KC_S,    KC_D,    KC_F,    KC_G,             KC_H,   KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,
              CTL_T(KC_RBRC), KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,             DF(_GAME),   KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, KC_RGUI,
              KC_LCTL,          KC_LGUI, CK_EMHS,  LT(_LOWER, KC_SPC),                          CK_KHKR,           KC_BSPC,          KC_RCTL
